156d8be1ad3f9bf7501737af077974151d052a3a
Aleksei Ivanov <alekseqolegovi26ivanov@yandex.ru>
2024-08-30 18:54:31 +0300

    End

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(
+                "Enter the first and the Second number | Введите первое и второе число - ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(
+                "Choose and Enter the type of operation you want to perform | Выберите и введите тип операции, которую вы хотите выполнить (+, -, *, /, %) - ");
+        char op = sc.next().charAt(0);
+        solve(a, b, op);
+    }
+    public static int solve(int a, int b, char op)
+    {
+        int ans = 0;
+
+        if (op == '+') {
+            ans = a + b;
         }
+        else if (op == '-') {
+            ans = a - b;
+        }
+        else if (op == '*') {
+            ans = a * b;
+        }
+        else if (op == '%') {
+            ans = a % b;
+        }
+        else if (op == '/') {
+            ans = a / b;
+        }
+
+        System.out.println("Your answer is - " + ans);
+        return ans;
     }
 }
